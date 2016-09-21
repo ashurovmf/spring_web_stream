f8bc1253a55386636e49bbb0dae98cbc7047aa03
Mikhail Ashurov <miav@gft.com>
2016-09-21 10:29:58 +0200

    Config tests were added.

--- src/test/java/com/gft/backend/configs/CustomWebInitializerTest.java
+++ src/test/java/com/gft/backend/configs/CustomWebInitializerTest.java
@@ -3,6 +3,8 @@ package com.gft.backend.configs;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by miav on 2016-09-08.
@@ -14,5 +16,17 @@ public class CustomWebInitializerTest {
         assertEquals(SpringRootConfig.class,initializer.getRootConfigClasses()[0]);
         assertEquals(SpringWebConfig.class,initializer.getServletConfigClasses()[0]);
         assertEquals("/",initializer.getServletMappings()[0]);
+    }
+
+    @Test
+    public void tryToInitWebSession() throws Exception {
+        SpringSessionInitializer initializer = new SpringSessionInitializer();
+        assertTrue("Session initializer is created", initializer != null);
+    }
+
+    @Test
+    public void tryToInitWeb() throws Exception {
+        CustomWebInitializer initializer = new CustomWebInitializer();
+        assertTrue("Custom web initializer is created", initializer != null);
     }
 }
